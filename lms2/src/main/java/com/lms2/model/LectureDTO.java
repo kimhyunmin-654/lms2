e0806ffe85dce40c5290ea9e6093679357deff5f
OH-hs95 <cofee955@naver.com>
2025-07-11 14:05:11 +0900

    강의 dto 공지사항dto 교수과제dto 학생과제dto

---
+++ lms2/src/main/java/com/lms2/model/LectureDTO.java
@@ -0,0 +1,117 @@
+package com.lms2.model;
+
+public class LectureDTO {
+	private String lecture_code;
+	private String subject;
+	private int grade;
+	private String classroom;
+	private String division;
+	private int lecture_year;
+	private String semester;
+	private int capacity;
+	private double credit;
+	private String department_id;
+	private String member_id;
+	private int file_id;
+	private String save_filename;
+	private String original_filename;
+	private int file_size;
+	private String reg_date;
+	
+	public int getFile_id() {
+		return file_id;
+	}
+	public void setFile_id(int file_id) {
+		this.file_id = file_id;
+	}
+	public String getSave_filename() {
+		return save_filename;
+	}
+	public void setSave_filename(String save_filename) {
+		this.save_filename = save_filename;
+	}
+	public String getOriginal_filename() {
+		return original_filename;
+	}
+	public void setOriginal_filename(String original_filename) {
+		this.original_filename = original_filename;
+	}
+	public int getFile_size() {
+		return file_size;
+	}
+	public void setFile_size(int file_size) {
+		this.file_size = file_size;
+	}
+	public String getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(String reg_date) {
+		this.reg_date = reg_date;
+	}
+	public String getLecture_code() {
+		return lecture_code;
+	}
+	public void setLecture_code(String lecture_code) {
+		this.lecture_code = lecture_code;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public String getClassroom() {
+		return classroom;
+	}
+	public void setClassroom(String classroom) {
+		this.classroom = classroom;
+	}
+	public String getDivision() {
+		return division;
+	}
+	public void setDivision(String division) {
+		this.division = division;
+	}
+	public int getLecture_year() {
+		return lecture_year;
+	}
+	public void setLecture_year(int lecture_year) {
+		this.lecture_year = lecture_year;
+	}
+	public String getSemester() {
+		return semester;
+	}
+	public void setSemester(String semester) {
+		this.semester = semester;
+	}
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	public double getCredit() {
+		return credit;
+	}
+	public void setCredit(double credit) {
+		this.credit = credit;
+	}
+	public String getDepartment_id() {
+		return department_id;
+	}
+	public void setDepartment_id(String department_id) {
+		this.department_id = department_id;
+	}
+	public String getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+}
---
+++ lms2/src/main/java/com/lms2/model/Pro_hwDTO.java
@@ -0,0 +1,90 @@
+package com.lms2.model;
+
+public class Pro_hwDTO {
+	private int homework_id;
+	private String subject;
+	private String content;
+	private String reg_date;
+	private String modify_date;
+	private int hit_count;
+	private String deadline_date;
+	private String lecture_code;
+	private int file_id;
+	private String save_filename;
+	private String original_filename;
+	private int file_size;
+	
+	public int getHomework_id() {
+		return homework_id;
+	}
+	public void setHomework_id(int homework_id) {
+		this.homework_id = homework_id;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(String reg_date) {
+		this.reg_date = reg_date;
+	}
+	public String getModify_date() {
+		return modify_date;
+	}
+	public void setModify_date(String modify_date) {
+		this.modify_date = modify_date;
+	}
+	public int getHit_count() {
+		return hit_count;
+	}
+	public void setHit_count(int hit_count) {
+		this.hit_count = hit_count;
+	}
+	public String getDeadline_date() {
+		return deadline_date;
+	}
+	public void setDeadline_date(String deadline_date) {
+		this.deadline_date = deadline_date;
+	}
+	public String getLecture_code() {
+		return lecture_code;
+	}
+	public void setLecture_code(String lecture_code) {
+		this.lecture_code = lecture_code;
+	}
+	public int getFile_id() {
+		return file_id;
+	}
+	public void setFile_id(int file_id) {
+		this.file_id = file_id;
+	}
+	public String getSave_filename() {
+		return save_filename;
+	}
+	public void setSave_filename(String save_filename) {
+		this.save_filename = save_filename;
+	}
+	public String getOriginal_filename() {
+		return original_filename;
+	}
+	public void setOriginal_filename(String original_filename) {
+		this.original_filename = original_filename;
+	}
+	public int getFile_size() {
+		return file_size;
+	}
+	public void setFile_size(int file_size) {
+		this.file_size = file_size;
+	}
+	
+}
