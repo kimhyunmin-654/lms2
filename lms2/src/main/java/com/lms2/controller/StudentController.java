2d191955eca0362366917cc9a98e88ac4e943a4e
haupyon <howni0630@gmail.com>
2025-07-11 16:43:48 +0900

    김하은 - 학생DAO 추가

--- lms2/src/main/java/com/lms2/controller/StudentController.java
+++ lms2/src/main/java/com/lms2/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.lms2.controller;
 
 import java.io.IOException;
 
+import com.lms2.dao.StudentDAO;
 import com.lms2.mvc.annotation.Controller;
 import com.lms2.mvc.annotation.RequestMapping;
 import com.lms2.mvc.annotation.RequestMethod;
@@ -10,6 +11,7 @@ import com.lms2.mvc.view.ModelAndView;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class StudentController {
@@ -18,15 +20,6 @@ public class StudentController {
 	public String loginForm(HttpServletRequest req, HttpServletRequest resp) throws ServletException, IOException {
 		
 		return "member/login";
-	}
-	
-	@RequestMapping(value = "/member/login", method = RequestMethod.POST)
-	public ModelAndView loginSubmit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// 로그인
-		
-		
-		
-		return null;
 	}
 	
 	@RequestMapping(value = "/member/logout", method = RequestMethod.GET)
