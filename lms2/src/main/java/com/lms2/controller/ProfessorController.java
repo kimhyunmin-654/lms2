0857960e2c8a1e0c51aec6fe79abd1f7cec71974
Hjy02 <ckach13@gmail.com>
2025-07-11 17:18:59 +0900

    황정연 - 교수 DAO, Controller 추가

---
+++ lms2/src/main/java/com/lms2/controller/ProfessorController.java
@@ -0,0 +1,40 @@
+package com.lms2.controller;
+
+import java.io.IOException;
+
+import com.lms2.mvc.annotation.RequestMethod;
+import com.lms2.mvc.annotation.Controller;
+import com.lms2.mvc.annotation.RequestMapping;
+import com.lms2.mvc.view.ModelAndView;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Controller
+public class ProfessorController {
+	@RequestMapping(value = "/member/login", method = RequestMethod.GET)
+	public String loginFrom(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 로그인 폼
+		
+		return "";
+	}
+	
+	@RequestMapping(value = "/member/login", method = RequestMethod.POST)
+	public ModelAndView loginSubmit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 로그인 처리
+		
+		return null;
+	}
+	
+	@RequestMapping(value = "/member/logout", method = RequestMethod.GET)
+	public String logout(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 로그아웃
+		
+		return "redirect:/";
+	}
+	
+	
+	
+	
+}
