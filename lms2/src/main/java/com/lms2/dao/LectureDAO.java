528eff861fbb9599f74b822d87e3a0cc341ada6f
Hjy02 <ckach13@gmail.com>
2025-07-12 23:24:47 +0900

    황정연 - 강의DAO 수정

--- lms2/src/main/java/com/lms2/dao/LectureDAO.java
+++ lms2/src/main/java/com/lms2/dao/LectureDAO.java
@@ -2,7 +2,6 @@ package com.lms2.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import com.lms2.model.LectureDTO;
@@ -14,10 +13,7 @@ public class LectureDAO {
 	
 	public void insertLecture(LectureDTO dto) throws SQLException {
 		PreparedStatement pstmt = null;
-		ResultSet rs = null;
 		String sql;
-		
-	
 		
 		try {
 			sql = "INSERT INTO LECTURE(lecture_code, subject, grade, classroom, division, lecture_year, semester, capacity, credit, department_id, member_id)"
@@ -33,16 +29,45 @@ public class LectureDAO {
 			pstmt.setInt(6, dto.getLecture_year());
 			pstmt.setString(7, dto.getSemester());
 			pstmt.setInt(8, dto.getCapacity());
-			//pstmt.setInt(9, dto.getCredit());
+			pstmt.setString(9, dto.getCredit());
 			pstmt.setString(10, dto.getDepartment_id());
 			pstmt.setString(11, dto.getMember_id());
 			
+			pstmt.executeUpdate();
+			
+			pstmt.close();
+			pstmt = null;
+			
+			sql = "INSERT INTO LECTURE_FILE(file_id, save_filename, original_filename, file_size, lecture_code, reg_date) "
+		             + "VALUES(LECTURE_FILE_SEQ.NEXTVAL, ?, ?, ?, ?, SYSDATE)";
+
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, dto.getSava_filename());
+			pstmt.setString(2, dto.getOriginal_filename());
+			pstmt.setInt(3, dto.getFile_size());
+			pstmt.setString(4, dto.getLecture_code());
+			
+			pstmt.executeUpdate();
+			
+			conn.commit();
+			
 		
+		} catch (SQLException e) {
+			DBUtil.rollback(conn);
+			e.printStackTrace();
+			throw e;
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw e;
 		} finally {
 			DBUtil.close(pstmt);
+			
+			try {
+				conn.setAutoCommit(true);
+			} catch (Exception e2) {
+			
+			}
 		}
 		
 	}
