74b55ce046d96b88324205de535d055484b7cced
OH-hs95 <cofee955@naver.com>
2025-07-14 16:16:12 +0900

    오현석-교수 강의 자료실 등록 수정 삭제

--- lms2/src/main/java/com/lms2/controller/DataController.java
+++ lms2/src/main/java/com/lms2/controller/DataController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import com.lms2.dao.DataDAO;
 import com.lms2.model.DataDTO;
+import com.lms2.model.SessionInfo;
 import com.lms2.mvc.annotation.RequestMapping;
 import com.lms2.mvc.annotation.RequestMethod;
 import com.lms2.mvc.annotation.ResponseBody;
@@ -16,6 +17,7 @@ import com.lms2.util.MyUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 public class DataController {
 	
@@ -78,7 +80,7 @@ public class DataController {
 				articleUrl += "&" + query;
 			}
 			
-			String paging = util.paging(current_page, total_page, articleUrl);
+			String paging = util.paging(current_page, total_page, listUrl);
 			
 			mav.addObject("list", list);
 			mav.addObject("dataCount", dataCount);
@@ -99,48 +101,184 @@ public class DataController {
 	public ModelAndView writeForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		//자료실 글쓰기
 		ModelAndView mav = new ModelAndView("data/write");
-		
+		mav.addObject("mode", "write");
 		return mav;
 	}
 	
 	@RequestMapping(value = "/data/write", method = RequestMethod.POST)
 	public ModelAndView writeSubmit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		//자료실 글 저장
-		ModelAndView mav = new ModelAndView("data/write");
+		DataDAO dao = new DataDAO();
 		
-		return mav;
+		HttpSession session = req.getSession();
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+		
+		try {
+			DataDTO dto = new DataDTO();
+			
+			dto.setMember_id(info.getMember_id());
+			
+			dto.setSubject(req.getParameter("subject"));
+			dto.setContent(req.getParameter("content"));
+			
+			dao.insertData(dto);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return new ModelAndView("redirect:/data/list");
+		
 	}
 	
 	@RequestMapping(value = "/data/article", method = RequestMethod.GET)
 	public ModelAndView article(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		//자료실 글보기
-		ModelAndView mav = new ModelAndView("data/article");
+		DataDAO dao = new DataDAO();
+		MyUtil util = new MyUtil();
 		
-		return mav;
+		String page = req.getParameter("page");
+		String query = "page=" + page;
+		
+		try {
+			int data_id = Integer.parseInt(req.getParameter("data_id"));
+			String schType = req.getParameter("schType");
+			String kwd = req.getParameter("kwd");
+			if(schType == null) {
+				schType = "all";
+				kwd = "";
+			}
+			kwd = util.decodeUrl(kwd);
+			
+			if(kwd.length() != 0) {
+				query += "&schType=" + schType + "&kwd=" + util.encodeUrl(kwd);
+			}
+			//게시물가져오기
+			DataDTO dto = dao.findById(data_id);
+			if(dto == null) {
+				return new ModelAndView("redirect:/data/list?" + query);
+			}
+			dto.setContent(util.htmlSymbols(dto.getContent()));
+			
+			//이전글 다음글
+			DataDTO prevDto = dao.findByPrev(dto.getData_id(), schType, kwd);
+			DataDTO nextDto = dao.findByNext(dto.getData_id(), schType, kwd);
+			
+			ModelAndView mav = new ModelAndView("data/article");
+			
+			mav.addObject("dto", dto);
+			mav.addObject("page", page);
+			mav.addObject("query", query);
+			mav.addObject("prevDto", prevDto);
+			mav.addObject("nextDto", nextDto);
+			
+			return mav;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return new ModelAndView("redirect:/data/list?" + query);
 	}
 	
 	@RequestMapping(value = "/data/update", method = RequestMethod.GET)
 	public ModelAndView updateForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		//자료실 글수정 폼
-		ModelAndView mav = new ModelAndView("data/update");
+		DataDAO dao = new DataDAO();
 		
-		return mav;//리스트 페이지로 리턴
+		HttpSession session = req.getSession();
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+		
+		String page = req.getParameter("page");
+		
+		try {
+			int data_id = Integer.parseInt(req.getParameter("data_id"));
+			DataDTO dto = dao.findById(data_id);
+			
+			if(dto == null) {
+				return new ModelAndView("redirect:/data/list?page=" + page);
+			}
+			
+			//게시물 작성자가 아니면
+			if(! dto.getMember_id().equals(info.getMember_id())) {
+				return new ModelAndView("redirect:/data/list?page=" + page);
+			}
+			
+			ModelAndView mav = new ModelAndView("data/write");
+			
+			mav.addObject("dto", dto);
+			mav.addObject("page", page);
+			mav.addObject("mode", "update");
+			
+			return mav;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return new ModelAndView("redirect:/data/list?page=" + page);
 	}
 	
 	@RequestMapping(value = "/data/update", method = RequestMethod.POST)
 	public ModelAndView updateSubmit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		//자료실 글수정 완료
-		ModelAndView mav = new ModelAndView("data/update");
+		DataDAO dao = new DataDAO();
 		
-		return mav;//리스트페이지로 리턴
+		HttpSession session = req.getSession();
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+		
+		String page = req.getParameter("page");
+		
+		try {
+			DataDTO dto = new DataDTO();
+			
+			dto.setData_id(Integer.parseInt(req.getParameter("data_id")));
+			dto.setSubject(req.getParameter("subject"));
+			dto.setContent(req.getParameter("content"));
+			
+			dto.setMember_id(info.getMember_id());
+			
+			dao.updateData(dto);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return new ModelAndView("redirect:/data/list?page=" + page);
 	}
 	
 	@RequestMapping(value = "/data/delete", method = RequestMethod.GET)
 	public ModelAndView delete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//자료실 글수정 완료
-		ModelAndView mav = new ModelAndView("data/delete");
+		//자료실 글삭제
+		DataDAO dao = new DataDAO();
+		MyUtil util = new MyUtil();
 		
-		return mav;//리스트페이지로 리턴
+		HttpSession session = req.getSession();
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+		
+		String page = req.getParameter("page");
+		String query = "page=" + page;
+		
+		try {
+			int data_id = Integer.parseInt(req.getParameter("data_id"));
+			
+			String schType = req.getParameter("schType");
+			String kwd = req.getParameter("kwd");
+			if(schType == null) {
+				schType = "all";
+				kwd = "";
+			}
+			kwd = util.decodeUrl(kwd);
+			
+			if(kwd.length() != 0) {
+				query += "&schType=" + schType + "&kwd=" + util.encodeUrl(kwd);
+			}
+			
+			dao.deleteData(data_id, info.getMember_id());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return new ModelAndView("redirect:/data/list?" + query);
 	}
 	
 	@RequestMapping(value = "/data/listReply", method = RequestMethod.GET)
--- lms2/src/main/java/com/lms2/dao/DataDAO.java
+++ lms2/src/main/java/com/lms2/dao/DataDAO.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.crypto.Data;
 
 import com.lms2.model.DataDTO;
 import com.lms2.util.DBConn;
@@ -15,22 +14,22 @@ import com.lms2.util.DBUtil;
 
 public class DataDAO {
 	private Connection conn = DBConn.getConnection();
-	
-	//데이터 추가
+
+	// 데이터 추가
 	public void insertData(DataDTO dto) throws SQLException {
 		PreparedStatement pstmt = null;
 		String sql;
-		
+
 		try {
 			sql = "INSERT INTO data(data_id, subject, content, hit_count, reg_date, modify_date, lecture_code)"
 					+ " VALUES(DATA_SEQ.NEXTVAL, ?, ?, 0, SYSDATE, SYSDATE, ?)";
-			
+
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setString(1, dto.getSubject());
 			pstmt.setString(2, dto.getContent());
 			pstmt.setString(3, dto.getLecture_code());
-			
+
 			pstmt.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -39,45 +38,45 @@ public class DataDAO {
 			DBUtil.close(pstmt);
 		}
 	}
-	
-	//데이터개수
+
+	// 데이터개수
 	public int dataCount() {
 		int result = 0;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		String sql;
-		
+
 		try {
 			sql = "SELECT COUNT (*) FROM DATA";
-			
+
 			pstmt = conn.prepareStatement(sql);
-			
+
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				result = rs.getInt(1);
 			}
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			DBUtil.close(rs);
 			DBUtil.close(pstmt);
 		}
-		
+
 		return result;
 	}
-	
-	//검색데이터 개수
+
+	// 검색데이터 개수
 	public int dataCount(String schType, String kwd) {
 		int result = 0;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		String sql;
-		
+
 		try {
 			sql = "SELECT COUNT(*) FROM DATA";
-			if(schType.equals("all")) {
+			if (schType.equals("all")) {
 				sql += " AND (INSTR(subject, ?) >= 1 OR INSTR(content, ?) >= 1 ) ";
 			} else if (schType.equals("reg_date")) {
 				kwd = kwd.replaceAll("(\\-|\\.|\\/)", "");
@@ -85,20 +84,20 @@ public class DataDAO {
 			} else {
 				sql += " AND INSTR(" + schType + ", ?) >= 1 ";
 			}
-			
+
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setString(1, kwd);
-			if(schType.equals("all")) {
+			if (schType.equals("all")) {
 				pstmt.setString(2, kwd);
 			}
-			
+
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				result = rs.getInt(1);
 			}
-					
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -107,34 +106,34 @@ public class DataDAO {
 		}
 		return result;
 	}
-	
-	//자료실 리스트
+
+	// 자료실 리스트
 	public List<DataDTO> listData(int offset, int size) {
 		List<DataDTO> list = new ArrayList<DataDTO>();
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		StringBuilder sb = new StringBuilder();
-		
+
 		try {
 			sb.append(" SELECT data_id, subject, TO_CHAR(reg_date, 'YYYY-MM-DD') reg_date, hit_count ");
 			sb.append(" FROM DATA");
 			sb.append(" ORDER BY data_id DESC ");
-			
+
 			pstmt = conn.prepareStatement(sb.toString());
-			
+
 			pstmt.setInt(1, offset);
 			pstmt.setInt(2, size);
-			
+
 			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				DataDTO dto = new DataDTO();
-				
+
 				dto.setData_id(rs.getInt("data_id"));
 				dto.setSubject(rs.getString("subject"));
 				dto.setReg_date(rs.getString("reg_date"));
 				dto.setHit_count(rs.getInt("hit_count"));
-				
+
 				list.add(dto);
 			}
 		} catch (Exception e) {
@@ -143,35 +142,35 @@ public class DataDAO {
 			DBUtil.close(rs);
 			DBUtil.close(pstmt);
 		}
-		
+
 		return list;
 	}
-	
+
 	public List<DataDTO> listData(int offset, int size, String schType, String kwd) {
 		List<DataDTO> list = new ArrayList<DataDTO>();
-		
+
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		StringBuilder sb = new StringBuilder();
-		
+
 		try {
 			sb.append(" SELECT data_id, subject, TO_CHAR(reg_date, 'YYYY-MM-DD') reg_date, hit_count ");
 			sb.append(" FROM DATA ");
-			if(schType.equals("all")) {
+			if (schType.equals("all")) {
 				sb.append(" WHERE INSTR(subject, ?) >= 1 ");
-			} else if(schType.equals("reg_date")) {
+			} else if (schType.equals("reg_date")) {
 				kwd = kwd.replaceAll("(\\-|\\.|\\/)", "");
 				sb.append(" WHERE TO_CHAR(reg_date, 'YYYYMMDD') = ? ");
 			} else {
 				sb.append(" WHERE INSTR(" + schType + ", ?) >= 1 ");
 			}
-			
+
 			sb.append(" ORDER BY data_id DESC ");
 			sb.append(" OFFSET ? ROWS FETCH FIRST ? ROWS ONLY ");
-			
+
 			pstmt = conn.prepareStatement(sb.toString());
-			
-			if(schType.equals("all")) {
+
+			if (schType.equals("all")) {
 				pstmt.setString(1, kwd);
 				pstmt.setInt(2, offset);
 				pstmt.setInt(3, size);
@@ -180,17 +179,17 @@ public class DataDAO {
 				pstmt.setInt(2, offset);
 				pstmt.setInt(3, size);
 			}
-			
+
 			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				DataDTO dto = new DataDTO();
-				
+
 				dto.setData_id(rs.getInt("data_id"));
 				dto.setSubject(rs.getString("subject"));
 				dto.setReg_date(rs.getString("reg_date"));
 				dto.setHit_count(rs.getInt("hit_count"));
-				
+
 				list.add(dto);
 			}
 		} catch (Exception e) {
@@ -199,20 +198,205 @@ public class DataDAO {
 			DBUtil.close(rs);
 			DBUtil.close(pstmt);
 		}
-		
+
 		return list;
 	}
-	
-	//조회수 증가
+
+	// 조회수 증가
 	public void updateHitCount(int data_id) throws SQLException {
+		PreparedStatement pstmt = null;
+		String sql;
+
+		try {
+			sql = "UPDATE DATA SET hit_count = hit_count + 1 WHERE data_id = ?";
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setInt(1, data_id);
+
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			DBUtil.close(pstmt);
+		}
+	}
+
+	// 해당 자료 보기
+	public DataDTO findById(int data_id) {
+		DataDTO dto = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql;
+
+		try {
+			sql = "SELECT data_id, subject, content, hit_count, reg_date, lecture_code " + " FROM DATA "
+					+ " WHERE NUM = ?";
+
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setLong(1, data_id);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				dto = new DataDTO();
+
+				dto.setData_id(rs.getInt("data_id"));
+				dto.setSubject(rs.getString("subject"));
+				dto.setContent(rs.getString("content"));
+				dto.setHit_count(rs.getInt("hit_count"));
+				dto.setReg_date(rs.getString("reg_date"));
+				dto.setLecture_code(rs.getString("lecture_code"));
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBUtil.close(rs);
+			DBUtil.close(pstmt);
+		}
+
+		return dto;
+	}
+
+	// 이전글
+	public DataDTO findByPrev(int data_id, String schType, String kwd) {
+		DataDTO dto = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			if (kwd != null && kwd.length() != 0) {
+				sb.append(" SELECT data_id, subject ");
+				sb.append(" FROM DATA ");
+				sb.append(" WHERE data_id < ? ");
+
+				if (schType.equals("all")) {
+					sb.append(" AND ( INSTR(subject, ?) >= 1 OR INSTR(content, ?) >= 1 ) ");
+				} else if ("reg_date".equals(schType)) {
+					kwd = kwd.replaceAll("(\\-|\\/|\\.)", "");
+					sb.append(" AND TO_CHAR(reg_date, 'YYYYMMDD') = ? ");
+				} else {
+					sb.append(" AND INSTR(" + schType + ", ?) >= 1 ");
+				}
+
+				sb.append(" ORDER BY data_id DESC ");
+				sb.append(" FETCH FIRST 1 ROWS ONLY ");
+
+				pstmt = conn.prepareStatement(sb.toString());
+				pstmt.setInt(1, data_id);
+
+				if (schType.equals("all")) {
+					pstmt.setString(2, kwd);
+					pstmt.setString(3, kwd);
+				} else {
+					pstmt.setString(2, kwd);
+				}
+			} else {
+				sb.append(" SELECT data_id, subject ");
+				sb.append(" FROM DATA ");
+				sb.append(" WHERE data_id < ? ");
+				sb.append(" ORDER BY data_id DESC ");
+				sb.append(" FETCH FIRST 1 ROWS ONLY ");
+
+				pstmt = conn.prepareStatement(sb.toString());
+				pstmt.setInt(1, data_id);
+			}
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				dto = new DataDTO();
+				dto.setData_id(rs.getInt("data_id"));
+				dto.setSubject(rs.getString("subject"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBUtil.close(rs);
+			DBUtil.close(pstmt);
+		}
+
+		return dto;
+	}
+
+
+
+	// 다음글
+	public DataDTO findByNext(int data_id, String schType, String kwd) {
+		DataDTO dto = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			if (kwd != null && kwd.length() != 0) {
+				sb.append(" SELECT data_id, subject ");
+				sb.append(" FROM DATA ");
+				sb.append(" WHERE data_id > ? ");
+
+				if (schType.equals("all")) {
+					sb.append(" AND ( INSTR(subject, ?) >= 1 OR INSTR(content, ?) >= 1 ) ");
+				} else if ("reg_date".equals(schType)) {
+					kwd = kwd.replaceAll("(\\-|\\/|\\.)", "");
+					sb.append(" AND TO_CHAR(reg_date, 'YYYYMMDD') = ? ");
+				} else {
+					sb.append(" AND INSTR(" + schType + ", ?) >= 1 ");
+				}
+
+				sb.append(" ORDER BY data_id ASC ");
+				sb.append(" FETCH FIRST 1 ROWS ONLY ");
+
+				pstmt = conn.prepareStatement(sb.toString());
+				pstmt.setInt(1, data_id);
+
+				if (schType.equals("all")) {
+					pstmt.setString(2, kwd);
+					pstmt.setString(3, kwd);
+				} else {
+					pstmt.setString(2, kwd);
+				}
+			} else {
+				sb.append(" SELECT data_id, subject ");
+				sb.append(" FROM DATA ");
+				sb.append(" WHERE data_id > ? ");
+				sb.append(" ORDER BY data_id ASC ");
+				sb.append(" FETCH FIRST 1 ROWS ONLY ");
+
+				pstmt = conn.prepareStatement(sb.toString());
+				pstmt.setInt(1, data_id);
+			}
+
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				dto = new DataDTO();
+				dto.setData_id(rs.getInt("data_id"));
+				dto.setSubject(rs.getString("subject"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DBUtil.close(rs);
+			DBUtil.close(pstmt);
+		}
+
+		return dto;
+	}
+	
+	public void updateData(DataDTO dto) throws SQLException {
 		PreparedStatement pstmt = null;
 		String sql;
 		
 		try {
-			sql = "UPDATE DATA SET hit_count = hit_count + 1 WHERE data_id = ?";
+			sql = "UPDATE DATA SET subject = ?, content = ? "
+					+ " WHERE data_id = ?";
 			pstmt = conn.prepareStatement(sql);
 			
-			pstmt.setInt(1, data_id);
+			pstmt.setString(1, dto.getSubject());
+			pstmt.setString(2, dto.getContent());
+			pstmt.setInt(3, dto.getData_id());
 			
 			pstmt.executeUpdate();
 		} catch (Exception e) {
@@ -220,25 +404,27 @@ public class DataDAO {
 			throw e;
 		} finally {
 			DBUtil.close(pstmt);
-		} 
+		}
 	}
 	
-	//해당 자료 보기
-	public DataDTO findById(int data_id) {
-		DataDTO dto = null;
+	public void deleteData(int data_id, String member_id) throws SQLException {
 		PreparedStatement pstmt = null;
-		ResultSet rs = null;
 		String sql;
 		
 		try {
-			sql = "SELECT ";
+			sql = "DELETE FROM DATA WHERE data_id = ? AND member_id = ?";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setInt(1, data_id);
+			pstmt.setString(2, member_id);
+			
+			pstmt.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
+			throw e;
 		} finally {
-			DBUtil.close(rs);
 			DBUtil.close(pstmt);
 		}
-		
-		return dto;
 	}
+	
 }
