1ef8a125c05cb0d7ee066d4ce55640c11edbb4cd
haupyon <howni0630@gmail.com>
2025-07-16 13:55:07 +0900

    김하은 - 괄호 수정

--- lms2/src/main/java/com/lms2/controller/StudentController.java
+++ lms2/src/main/java/com/lms2/controller/StudentController.java
@@ -24,39 +24,41 @@ import jakarta.servlet.http.HttpSession;
 @Controller
 public class StudentController {
 	@RequestMapping(value = "/admin/student/list", method = RequestMethod.GET)
-	public ModelAndView studentList(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	public ModelAndView studentList(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
 		// 학생 리스트
 		StudentDAO dao = new StudentDAO();
 		MyUtil util = new MyUtil();
-		
+
 		ModelAndView mav = new ModelAndView("admin/student/list");
-		
+
 		try {
 			String page = req.getParameter("page");
 			int current_page = 1;
-			if(page != null) {
+			if (page != null) {
 				current_page = Integer.parseInt(page);
 			}
-			
+
 			int dataCount = dao.dataCount();
-			
+
 			int size = 10;
 			int total_page = util.pageCount(dataCount, size);
-			if(current_page > total_page) {
+			if (current_page > total_page) {
 				current_page = total_page;
 			}
-			
+
 			int offset = (current_page - 1) * size;
-			if(offset < 0) offset = 0;
-			
+			if (offset < 0)
+				offset = 0;
+
 			List<StudentDTO> list = dao.listStudent(offset, size);
-			
+
 			String cp = req.getContextPath();
 			String listUrl = cp + "/admin/student/list";
 			String articleUrl = cp + "admin/article?page=" + current_page;
-			
+
 			String paging = util.paging(current_page, total_page, listUrl);
-			
+
 			mav.addObject("list", list);
 			mav.addObject("dataCount", dataCount);
 			mav.addObject("size", size);
@@ -64,36 +66,38 @@ public class StudentController {
 			mav.addObject("total_page", total_page);
 			mav.addObject("articleUrl", articleUrl);
 			mav.addObject("paging", paging);
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return mav;
 	}
-	
+
 	@RequestMapping(value = "/admin/student/account", method = RequestMethod.GET)
-	public ModelAndView accountStudentForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	public ModelAndView accountStudentForm(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
 		// 학생 등록 폼
 		ModelAndView mav = new ModelAndView("admin/student/account");
 		mav.addObject("mode", "account");
-		
+
 		return mav;
 	}
-	
+
 	@RequestMapping(value = "/admin/student/account", method = RequestMethod.POST)
-	public ModelAndView accountStudentSubmit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	public ModelAndView accountStudentSubmit(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
 		// 학생 등록
 		StudentDAO dao = new StudentDAO();
-		
+
 		// 사진 파일은 나중에... -김하은
-		
+
 		HttpSession session = req.getSession();
-		
+
 		String message = "";
-		
+
 		try {
 			StudentDTO dto = new StudentDTO();
-			
+
 			dto.setMember_id(req.getParameter("userId"));
 			dto.setName(req.getParameter("name"));
 			dto.setPassword(req.getParameter("password"));
@@ -102,21 +106,21 @@ public class StudentController {
 			dto.setBirth(req.getParameter("birth"));
 			dto.setAddr1(req.getParameter("addr1"));
 			dto.setAddr2(req.getParameter("addr2"));
-			
+
 			dto.setGrade(Integer.parseInt(req.getParameter("grade")));
 			dto.setAdmission_date(req.getParameter("admission_date"));
 			dto.setDepartment_id(req.getParameter("department_id"));
-			
+
 			dao.insertStudent(dto);
-			
+
 			session.setAttribute("mode", "account");
-			
+
 			return new ModelAndView("redirect:/admin/student/list");
-			
+
 		} catch (SQLException e) {
-			if(e.getErrorCode() == 1) {
+			if (e.getErrorCode() == 1) {
 				message = "이미 등록된 학번입니다.";
-			} else if(e.getErrorCode() == 1840 || e.getErrorCode() == 1861) {
+			} else if (e.getErrorCode() == 1840 || e.getErrorCode() == 1861) {
 				message = "날짜 형식이 올바르지 않습니다.";
 			} else {
 				message = "학생 등록에 실패했습니다.";
@@ -125,83 +129,85 @@ public class StudentController {
 			message = "학생 등록에 실패했습니다.";
 			e.printStackTrace();
 		}
-		
+
 		ModelAndView mav = new ModelAndView("admin/student/account");
 		mav.addObject("mode", "account");
 		mav.addObject("message", message);
-		
+
 		return mav;
 	}
-	
+
 	@RequestMapping(value = "/admin/student/complete", method = RequestMethod.GET)
-	public ModelAndView studentComplete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	public ModelAndView studentComplete(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
 		// 학생 등록 완료 후
 		HttpSession session = req.getSession();
-		
-		String mode = (String)session.getAttribute("mode");
-		String userName = (String)session.getAttribute("name");
-		
+
+		String mode = (String) session.getAttribute("mode");
+		String userName = (String) session.getAttribute("name");
+
 		session.removeAttribute("mode");
 		session.removeAttribute("name");
-		
-		if(mode == null) {
+
+		if (mode == null) {
 			return new ModelAndView("redirect:/");
 		}
-		
+
 		String title;
 		String message = "<b>" + userName + "</b>님 ";
-		
-		if(mode.equals("account")) {
+
+		if (mode.equals("account")) {
 			title = "학생등록";
 			message += "학생 등록이 완료 되었습니다.";
 		} else {
 			title = "학생 정보 수정";
 			message += "학생 정보 수정이 완료되었습니다.";
 		}
-		
+
 		ModelAndView mav = new ModelAndView("admin/student/complete");
-		
+
 		mav.addObject("title", title);
 		mav.addObject("message", message);
-		
+
 		return mav;
 	}
 
-	
 	@RequestMapping(value = "/student/lecture/list", method = RequestMethod.GET)
-	public ModelAndView lectureList(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	public ModelAndView lectureList(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
 		// 강의 리스트
 		LectureDAO dao = new LectureDAO();
 		MyUtil util = new MyUtil();
-		
+
 		ModelAndView mav = new ModelAndView("student/lecture/list");
-		
+
 		try {
 			String page = req.getParameter("page");
 			int current_page = 1;
-			if(page != null) {
+			if (page != null) {
 				current_page = Integer.parseInt(page);
 			}
-			
+
 			int dataCount = dao.dataCount();
-			
+
 			int size = 10;
 			int total_page = util.pageCount(dataCount, size);
-			if(current_page > total_page) {
+			if (current_page > total_page) {
 				current_page = total_page;
 			}
-			
+
 			int offset = (current_page - 1) * size;
-			if(offset < 0) offset = 0;
-			
+			if (offset < 0)
+				offset = 0;
+
 			List<LectureDTO> list = dao.listLecture(offset, size);
-			
+
 			String cp = req.getContextPath();
 			String listUrl = cp + "/student/lecture/list";
 			String articleUrl = cp + "/student/lecture/view?page=" + current_page;
-			
+
 			String paging = util.paging(current_page, total_page, listUrl);
-			
+
 			mav.addObject("list", list);
 			mav.addObject("dataCount", dataCount);
 			mav.addObject("size", size);
@@ -209,21 +215,22 @@ public class StudentController {
 			mav.addObject("total_page", total_page);
 			mav.addObject("articleUrl", articleUrl);
 			mav.addObject("paging", paging);
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return mav;
 	}
-	
+
 	@RequestMapping(value = "/student/lecture/account", method = RequestMethod.POST)
-	public ModelAndView lectureSubmit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	public ModelAndView lectureSubmit(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
 		// 수강 신청 (수정중)
 		StudentDAO dao = new StudentDAO();
-		
+
 		HttpSession session = req.getSession();
 		SessionInfo info = (SessionInfo) session.getAttribute("student");
-		
+
 		try {
 			StudentDTO dto = new StudentDTO();
 			String lectureCode = req.getParameter("lectureCode");
@@ -231,16 +238,13 @@ public class StudentController {
 			dto.setLecture_code(req.getParameter("lecture_code"));
 			dto.setApply_status("신청");
 
-			dao.insertCourse(dto);
-			
+			// dao.insertCourse(dto);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return new ModelAndView("redirect:/student/lecture/list");
 	}
 
-	
-	
-	
-
+}
