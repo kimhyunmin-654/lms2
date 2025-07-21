01ad2a53bc24f2eeacdb65e2b0c9761fcf2a2960
hyeonmin <hyeonmin8399@naver.com>
2025-07-21 16:51:23 +0900

    김현민 - 학생 메인페이지 수정

--- lms2/src/main/java/com/lms2/controller/StudentController.java
+++ lms2/src/main/java/com/lms2/controller/StudentController.java
@@ -53,7 +53,7 @@ public class StudentController {
 			mav.addObject("list", list);
 
 			// 공지사항
-			List<NoticeDTO> listNotice = noticeDao.listNotice();
+			List<NoticeDTO> listNotice = noticeDao.listNotice(0,5);
 			mav.addObject("listNotice", listNotice);
 
 		} catch (Exception e) {
